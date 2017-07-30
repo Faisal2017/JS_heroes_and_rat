b669a145d7c50cde682a45425b29d3b05574ddc6
Faisal <faisal_a@live.co.uk>
2017-07-30 19:51:00 +0100

    test for food increasing hero's health passes

--- hero.js
+++ hero.js
@@ -9,6 +9,9 @@ Hero.prototype.talk = function() {
   return "Hello, my name is " + this.name;
 }
 
+Hero.prototype.eat = function(food) {
+  return this.health += food.healingValue;  
+}
 
 
 module.exports = Hero;
